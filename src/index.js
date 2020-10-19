b212548b36a32446091ecd6f4374985b999afb85
yarnax <feniks_63@mail.ru>
2020-10-19 16:17:09 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+  let textNumber = number.toString();
+  let answer = '';
+  for (chr in textNumber) {
+    switch (chr) {
+      case "1":
+        answer += ""
+    }
+
+  }
 }
